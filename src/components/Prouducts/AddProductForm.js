310cfddf8ac509b1fc21eb3b29667766e2493e94
Bùi Minh Tuấn <92067348+tuan20520342@users.noreply.github.com>
2023-03-23 23:56:37 +0700

    product file list

--- src/components/Prouducts/AddProductForm.js
+++ src/components/Prouducts/AddProductForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import dayjs from 'dayjs';
 import { PlusOutlined } from '@ant-design/icons';
+import ImgCrop from 'antd-img-crop';
 import {
   Form,
   Input,
@@ -27,6 +28,14 @@ const { Option } = Select;
 const { TextArea } = Input;
 const dateFormat = 'DD/MM/YYYY';
 
+const getBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
 const validateMessages = {
   required: 'Cần nhập ${label}!',
   types: {
@@ -42,7 +51,55 @@ const validateMessages = {
 const AddProductForm = () => {
   const navigate = useNavigate();
   const [form] = Form.useForm();
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+  const [previewTitle, setPreviewTitle] = useState('');
   const [imageChange, setImageChange] = useState('');
+  const [mainFileList, setMainFileList] = useState([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+  ]);
+  const [fileList, setFileList] = useState([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+  ]);
+  const handleCancel = () => setPreviewOpen(false);
+  const handlePreview = async (file) => {
+    console.log(file);
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+    setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
+  };
+  const handleChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+  const handleMainChange = ({ fileList: newFileList }) => {
+    setMainFileList(newFileList);
+  };
+
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  );
   const handleClose = () => {
     navigate('/products');
   };
@@ -83,7 +140,7 @@ const AddProductForm = () => {
               </Col>
               <Col span={24}>
                 <Form.Item
-                  name="staff_name"
+                  name="name"
                   label="Tên sản phẩm"
                   rules={[
                     {
@@ -95,7 +152,7 @@ const AddProductForm = () => {
                 </Form.Item>
               </Col>
               <Col span={24}>
-                <Form.Item name="staff_address" label="Mô tả" rules={[{ required: true }]}>
+                <Form.Item name="description" label="Mô tả" rules={[{ required: true }]}>
                   <TextArea rows={2} placeholder="Mô tả" />
                 </Form.Item>
               </Col>
@@ -114,7 +171,7 @@ const AddProductForm = () => {
                 <Title level={4}>Giá sản phẩm</Title>
               </Col>
 
-              <Col xs={24} sm={12} md={24} lg={12}>
+              {/* <Col xs={24} sm={12} md={24} lg={12}>
                 <Form.Item
                   name="cost"
                   label="Giá nhập"
@@ -133,8 +190,8 @@ const AddProductForm = () => {
                     parser={(value) => parseInt(value.replace(/\$\s?|(,*)/g, ''))}
                   />
                 </Form.Item>
-              </Col>
-              <Col xs={24} sm={12} md={24} lg={12}>
+              </Col> */}
+              <Col span={24}>
                 <Form.Item
                   name="price"
                   label="Giá bán"
@@ -166,132 +223,100 @@ const AddProductForm = () => {
               }}
             >
               <Col span={24}>
-                <Title level={4}>Số lượng sản phẩm</Title>
-              </Col>
-
-              <Col span={12}>
-                <Form.Item
-                  name="staff_name"
-                  label="Tên sản phẩm"
-                  rules={[
-                    {
-                      required: true,
-                    },
-                  ]}
-                >
-                  <Input placeholder="Tên sản phẩm" />
-                </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Form.Item name="staff_address" label="Mô tả" rules={[{ required: true }]}>
-                  <TextArea rows={2} placeholder="Mô tả" />
-                </Form.Item>
-              </Col>
-            </Row>
-          </div>
-          <div style={{ background: 'white', borderRadius: '6px', marginBottom: '16px', padding: '0 20px' }}>
-            <Row
-              gutter={{
-                xs: 8,
-                sm: 16,
-                md: 24,
-                lg: 32,
-              }}
-            >
-              <Col span={24}>
                 <Title level={4}>Danh mục sản phẩm</Title>
               </Col>
 
-              <Col span={12}>
-                <Form.Item
-                  name="staff_name"
-                  label="Tên sản phẩm"
-                  rules={[
-                    {
-                      required: true,
-                    },
-                  ]}
-                >
-                  <Input placeholder="Tên sản phẩm" />
-                </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Form.Item name="staff_address" label="Mô tả" rules={[{ required: true }]}>
-                  <TextArea rows={2} placeholder="Mô tả" />
-                </Form.Item>
-              </Col>
-            </Row>
-          </div>
-          <div style={{ background: 'white', borderRadius: '6px', padding: '0 20px' }}>
-            <Row
-              gutter={{
-                xs: 8,
-                sm: 16,
-                md: 24,
-                lg: 32,
-              }}
-            >
               <Col span={24}>
-                <Title level={4}>Hình ảnh</Title>
-              </Col>
-
-              <Col span={12}>
                 <Form.Item
-                  name="staff_name"
-                  label="Tên sản phẩm"
+                  name="categories"
+                  label="Danh mục"
                   rules={[
                     {
                       required: true,
                     },
                   ]}
                 >
-                  <Input placeholder="Tên sản phẩm" />
-                </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Form.Item name="staff_address" label="Mô tả" rules={[{ required: true }]}>
-                  <TextArea rows={2} placeholder="Mô tả" />
+                  <Select
+                    showSearch
+                    placeholder="Danh mục"
+                    allowClear
+                    // options={options}
+                    filterOption={(input, option) => (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
+                  ></Select>
                 </Form.Item>
               </Col>
             </Row>
           </div>
         </Col>
         <Col xs={24} sm={8} md={24} lg={8}>
-          <Row
-            style={{
-              background: 'white',
-              borderRadius: '6px',
-              // filter: 'drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))',
-            }}
-            // gutter={{
-            //   xs: 8,
-            //   sm: 16,
-            //   md: 24,
-            //   lg: 32,
-            // }}
-          >
-            <Col span={24}>
-              <Title level={4}>Thông tin sản phẩm</Title>
-            </Col>
-            <Col span={24}>
-              <Form.Item
-                name="staff_name"
-                label="Tên sản phẩm"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
-              >
-                <Input placeholder="Tên sản phẩm" />
-              </Form.Item>
-            </Col>
-            <Col span={24}>
-              <Form.Item name="staff_address" label="Mô tả" rules={[{ required: true }]}>
-                <TextArea rows={2} placeholder="Mô tả" />
-              </Form.Item>
-            </Col>
-          </Row>
+          <div style={{ background: 'white', borderRadius: '6px', marginBottom: '16px', padding: '0 20px' }}>
+            <Row>
+              <Col span={24}>
+                <Title level={4}>Hình ảnh sản phẩm</Title>
+              </Col>
+              <Col span={24}>
+                <Form.Item name="mainImage" label="Ảnh chính">
+                  <ImgCrop rotationSlider>
+                    <Upload
+                      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                      listType="picture-card"
+                      fileList={mainFileList}
+                      onPreview={handlePreview}
+                      onChange={handleMainChange}
+                      style={{ display: 'inline-block' }}
+                    >
+                      {mainFileList.length >= 1 ? null : uploadButton}
+                    </Upload>
+                  </ImgCrop>
+                  <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+                    <img
+                      alt="example"
+                      style={{
+                        width: '100%',
+                      }}
+                      src={previewImage}
+                    />
+                  </Modal>
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item name="images" label="Thêm ảnh mô tả">
+                  <ImgCrop rotationSlider>
+                    <Upload
+                      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                      listType="picture-card"
+                      fileList={fileList}
+                      onPreview={handlePreview}
+                      onChange={handleChange}
+                    >
+                      {fileList.length >= 3 ? null : uploadButton}
+                    </Upload>
+                  </ImgCrop>
+                  <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+                    <img
+                      alt="example"
+                      style={{
+                        width: '100%',
+                      }}
+                      src={previewImage}
+                    />
+                  </Modal>
+                </Form.Item>
+              </Col>
+            </Row>
+          </div>
+          <div style={{ background: 'white', borderRadius: '6px', marginBottom: '16px', padding: '20px' }}>
+            <Row justify="center">
+              <Space>
+                <Button size="large" type="primary" htmlType="submit">
+                  Xác nhận
+                </Button>
+                <Button size="large" type="primary" danger onClick={handleClose}>
+                  Đóng
+                </Button>
+              </Space>
+            </Row>
+          </div>
         </Col>
       </Row>
 
