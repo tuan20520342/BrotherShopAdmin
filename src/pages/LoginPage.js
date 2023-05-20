a06c1daeed2c59ff9117fca33c97cd5cf062692d
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-05-20 17:34:08 +0700

    handle expired token

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,14 +1,18 @@
 import { Col, Row } from 'antd';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import LoginForm from '~/components/Login/LoginForm';
+import AlertCustom from '~/components/UI/Notification/Alert';
 
 const LoginPage = () => {
   const { state } = useLocation();
-  // useEffect(() => {
-  //   if (state) {
-  //     AlertCustom({ type: 'loginToContinue', title: 'Hãy đăng nhập để tiếp tục' });
-  //   }
-  // }, [state]);
+
+  useEffect(() => {
+    if (state) {
+      AlertCustom({ type: 'loginToContinue', title: state.message });
+    }
+  }, [state]);
+
   return (
     <>
       <Row style={{ minHeight: '100vh', backgroundColor: '#d0ebff' }} justify="center">
