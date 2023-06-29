1a0a5473aad3620df5c72b66d8deedb67a723bdf
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-06-29 21:07:43 +0700

    replace base request by axios

--- src/services/api/AuthAPI.js
+++ src/services/api/AuthAPI.js
@@ -1,11 +1,11 @@
 import * as UrlApi from '../url';
-import baseRequest from './BaseRequest';
+import axios from 'axios';
 
 export const AuthenticationService = {
   postLogin: (user) => {
     let { username, password } = user;
 
-    return baseRequest.post(UrlApi.URL_AUTH_LOGIN, {
+    return axios.post(UrlApi.URL_AUTH_LOGIN, {
       username: username,
       password: password,
     });
